89211bd8d3bd755a6e7bb4573b0ec126be32f7b0
kiemthd <htk2090500@gmail.com>
2024-12-04 14:25:35 +0700

    Add files via upload

---
+++ Bài 4.1.py
@@ -0,0 +1,7 @@
+print('Hoàng Thanh Kiếm MSSV 235752021610003')
+# Nhập chuỗi S từ bàn phím
+S = input("Nhập chuỗi S: ")
+
+# In ra từng kí tự của S, mỗi kí tự trên một dòng
+for char in S:
+    print(char)
